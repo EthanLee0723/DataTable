4ecd7c078946ee0abc2c125255311d20b26ac31c
Ethan Lee <71550364+EthanLee0723@users.noreply.github.com>
2024-02-28 00:52:48 +0800

    project setup

---
+++ frontend/src/app/layout.js
@@ -0,0 +1,12 @@
+import 'bootstrap/dist/css/bootstrap.css';
+import '/public/assets/css/dataTables.css';
+
+export default function RootLayout({ children }) {
+    return (
+        <html lang="en">
+            <body>
+                {children}
+            </body>
+        </html>
+    );
+}
